084f8732e156736933fae4a69bce1aa9f137382b
Alexander Kostyurenko <alekkostyur@gmail.com>
2019-09-17 10:34:47 +0300

    Added javadoc

--- src/main/java/org/turingsquad/station/StationDataReaderFromFile.java
+++ src/main/java/org/turingsquad/station/StationDataReaderFromFile.java
@@ -10,6 +10,16 @@ public abstract class StationDataReaderFromFile {
     protected final String fileName;
     protected boolean prepared;
 
+    /**
+     * Parse data from file
+     * @return List of {@link StationData} objects parsed from file
+     */
     public abstract List<StationData> read();
+
+    /**
+     * Prepare reader to parse data
+     * @return
+     * @throws IOException if file was not found
+     */
     public abstract StationDataReaderFromFile init() throws IOException;
 }
